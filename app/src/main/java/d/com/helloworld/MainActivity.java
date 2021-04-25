f7204704a4f0fd5cef062d30f7544e27180baeb5
Abax <abassife@gmail.com>
2021-04-25 17:59:59 +0100

    First commit

--- app/src/main/java/d/com/helloworld/MainActivity.java
+++ app/src/main/java/d/com/helloworld/MainActivity.java
@@ -10,25 +10,25 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private Button AddButton;
-    private int x=0;
+    private int increment=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
 
 
     }
 
     public void AddButton(View view)
     {
-        x +=1;
-        String text = String.valueOf(x);
-        textView.setText(text);
+        increment +=1;
+        String add = String.valueOf(increment);
+        textView.setText(add);
     }
 }
